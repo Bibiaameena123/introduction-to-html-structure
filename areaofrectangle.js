82f55c5584cf3c84fd52a53c0e5021b71fad2765
Bibiaameena123 <doddamanibibiaameena@gmail.com>
2025-05-10 11:40:52 -0700

    Add files via upload

---
+++ areaofrectangle.js
@@ -0,0 +1,10 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+  }
+  
+  // Example usage:
+  const length = 10;
+  const width = 5;
+  const area = calculateRectangleArea(length, width);
+  console.log("Area of the rectangle:", area);
+  
